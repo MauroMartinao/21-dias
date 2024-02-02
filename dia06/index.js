2c064745967295aa04883c7c6192ba076799c09b
Mauro-PC <mauromartinao@gmail.com>
2024-02-02 17:36:04 -0300

    dia06

---
+++ dia06/index.js
@@ -0,0 +1,33 @@
+// let tabuada = Number(prompt('A tabuada de qual numero voce deseja ver?'));
+// 
+// for (let contador = 0; contador <= 10; contador++){ 
+//     console.log(tabuada + " x " + contador + " = " + tabuada * contador)
+// }
+
+// Quiz 1
+//let value = Number(prompt('insira um numero'));
+//let i = 0
+//for (let i = 0; i <= value ; i++) {
+//    console.log(i);
+//}
+
+// Quizz 2
+//for (let i = 0; i <= 50 ; i += 5) {
+//    console.log(i)
+//}
+
+// Quizz 3
+//for (let i = 50; i >= 0 ; i -= 5) {
+//    console.log(i)
+//}
+
+// Quiz 4
+//
+//let num = parseInt(prompt("put a number"))
+//
+//for (let i = num ; i <= num + 2 ; i++){
+//    console.log("Tabuada do: " + i)
+//    for (let j = 0 ; j <= 10 ; j++){
+//        console.log(i + " x " + j + " = " + (i * j))
+//    }
+//}
