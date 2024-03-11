b007ce5bcbd4848d191eb1b11257ff8f8be16944
Mauro-PC <mauromartinao@gmail.com>
2024-03-11 15:29:58 -0300

    exerciciosDeArray

---
+++ dia10/index.js
@@ -0,0 +1,38 @@
+//const array = []
+//const indices = []
+//let contadorIndice = 0
+//
+//const numeroProcurado = parseInt(prompt("Digite o numero a ser procurado? "))
+//
+//for (let i = 0; i < 10 ; i++) {
+//    const elemento = parseInt(prompt(`Digite o elemento ${i + 1}:`));
+//    array[i] = elemento
+//}
+//
+//for (let i = 0; i < 10; i++){
+//    if (array[i] === numeroProcurado) {
+//        indices[contadorIndice] = 1
+//        contadorIndice++
+//    }
+//}
+//console.log(`O número ${numeroProcurado} foi encontrado nos índices: ${indices}`);
+
+
+
+//#2
+//const array = []
+//const arrayInvertido = []
+//
+//for (let i = 0 ; i < 5 ; i++) {
+//    let numero = parseInt(prompt("digite o " + (i + 1) + " numero"))
+//    array[i] = numero
+//}
+//console.log("Array original " + array)
+//
+//let contador = 4
+//for (let i = 0; i < 5; i++) {
+//   arrayInvertido[i] = array[contador]
+//   contador--    
+//}
+//console.log("Array invertida? " + arrayInvertido)
+
