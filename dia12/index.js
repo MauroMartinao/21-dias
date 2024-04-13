8ba8d71357f7e10426a6fe65c55b6ea540cb20d2
Mauro-PC <mauromartinao@gmail.com>
2024-04-13 19:00:55 -0300

    exercicios dia 12 e 13

--- dia12/index.js
+++ dia12/index.js
@@ -1,71 +1,136 @@
- class Aluno {
-     nomei
-     constructor(nome, idade, sexo) {
-         this.nome = nome
-         this.idade = idade
-         this.sexo = sexo
-     }
- }
- 
- console.log("======CADASTRO DE ALUNOS======")
- let alunos = [];
- let continuar = true;
- let indexDoAluno = 0; 
- 
- while (continuar) {
-     let nome = prompt("insira o nome")
-     let idade = prompt("insira a idade")
-     let sexo = prompt("insira o sexo")
-     let aluno = new Aluno(nome, idade, sexo)
- 
-     alunos[indexDoAluno] = aluno
-     let desejaContinuar = prompt("Insira 1 para cadastrar novo aluno")
-     if (desejaContinuar != "1") {
-         continuar = false;
-     } else {
-         indexDoAluno++
-     }
- }
+//  class Aluno {
+//      nomei
+//      constructor(nome, idade, sexo) {
+//          this.nome = nome
+//          this.idade = idade
+//          this.sexo = sexo
+//      }
+//  }
+//  
+//  console.log("======CADASTRO DE ALUNOS======")
+//  let alunos = [];
+//  let continuar = true;
+//  let indexDoAluno = 0; 
+//  
+//  while (continuar) {
+//      let nome = prompt("insira o nome")
+//      let idade = prompt("insira a idade")
+//      let sexo = prompt("insira o sexo")
+//      let aluno = new Aluno(nome, idade, sexo)
+//  
+//      alunos[indexDoAluno] = aluno
+//      let desejaContinuar = prompt("Insira 1 para cadastrar novo aluno")
+//      if (desejaContinuar != "1") {
+//          continuar = false;
+//      } else {
+//          indexDoAluno++
+//      }
+//  }// 
 
- class Computador {
-    tipo
-    processador
-    video
-    armazenamento
-    memoriaRam
-    SSD
+// EXERCICIO 2
 
-    constructor(tipo, processador, video, armazenamento, memoriaRam, SSD){
-        this.tipo = tipo;
-        this.processador = processador;
-        this.video = video;
-        this.armazenamento = armazenamento;
-        this.memoriaRam = memoriaRam;
-        this.SSD = SSD;
-    }   
-}
+//  class Computador {
+//     tipo
+//     processador
+//     video
+//     armazenamento
+//     memoriaRam
+//     SSD// 
 
-console.log("=====MEU SETUP=====")
+//     constructor(tipo, processador, video, armazenamento, memoriaRam, SSD){
+//         this.tipo = tipo;
+//         this.processador = processador;
+//         this.video = video;
+//         this.armazenamento = armazenamento;
+//         this.memoriaRam = memoriaRam;
+//         this.SSD = SSD;
+//     }   
+// }// 
 
-let pcs = []
-let continuar = true
-let indexDoPc = 0
+// console.log("=====MEU SETUP=====")// 
 
-while (continuar) {
-    let tipo = prompt("Qual o seu tipo de computador?")
-    let processador = prompt("Qual o seu processador?")
-    let video = prompt("Qual a placa de video?")
-    let armazenamento = prompt("Qual o tipo de memoria de armazenamento?")
-    let memoriaRam = prompt("Quanto de memoria RAM?")
-    let SSD = prompt("Quanto de SSD?")
+// let pcs = []
+// let continuar = true
+// let indexDoPc = 0// 
 
-    let pc = new Computador(tipo, processador, video, armazenamento, memoriaRam, SSD)
-pcs[indexDoPc] = pc;
+// while (continuar) {
+//     let tipo = prompt("Qual o seu tipo de computador?")
+//     let processador = prompt("Qual o seu processador?")
+//     let video = prompt("Qual a placa de video?")
+//     let armazenamento = prompt("Qual o tipo de memoria de armazenamento?")
+//     let memoriaRam = prompt("Quanto de memoria RAM?")
+//     let SSD = prompt("Quanto de SSD?")// 
 
-    let outroCadastro = prompt("Cadastrar outro SETUP?")
-    if (outroCadastro != "1") {
-        continuar = false;
-    }else{
-        indexDoPc++
-    }
-}
+//     let pc = new Computador(tipo, processador, video, armazenamento, memoriaRam, SSD)
+// pcs[indexDoPc] = pc;// 
+
+//     let outroCadastro = prompt("Cadastrar outro SETUP?")
+//     if (outroCadastro != "1") {
+//         continuar = false;
+//     }else{
+//         indexDoPc++
+//     }
+// }
+
+
+
+// class Car{
+//     name 
+//     power
+//     maxSpeed
+//     acceleration
+// 
+//     constructor(name, power, maxSpeed, acceleration){
+//     this.name = name
+//     this.power = power
+//     this.maxSpeed = maxSpeed
+//     this.acceleration = acceleration
+//     }
+// 
+//     CalcularTempoMedio(distancia) {
+//         let resultado = distancia/(this.maxSpeed / this.acceleration)
+//         return resultado
+//     }
+// }
+// 
+
+// EXERCICIO 3
+
+// class Corrida {
+//     constructor(nome, tipo, distancia, participantes, vencedor){
+//         this.nome = nome;
+//         this.tipo = tipo;
+//         this.distancia = distancia;
+//         this.participantes = participantes; // Corrigido
+//         this.vencedor = vencedor; // Corrigido
+//     }
+// 
+//     DefinirVencedor(){
+//         let menorTempo = this.participantes[0].CalcularTempoMedio(this.distancia);
+//         let vencedor = this.participantes[0];
+// 
+//         for (let index = 1 ; index < this.participantes.length ; index++) {
+//             let tempo = this.participantes[index].CalcularTempoMedio(this.distancia); // Corrigido
+//             if (tempo < menorTempo) { // Adicionado
+//                 menorTempo = tempo; // Corrigido
+//                 vencedor = this.participantes[index]; // Corrigido
+//             }
+//         }
+// 
+//         return this.vencedor = vencedor; // Movido para dentro do método
+//     }
+// 
+//     ExibirVencedor(){
+//         alert("O vencedor é " + this.vencedor.nome); // Corrigido
+//     }
+// }
+// 
+// let corrida = new Corrida("Monza", "Formula 1", 60000, [], null); // Certifique-se de passar um array vazio para participantes e null para vencedor
+// 
+// corrida.participantes = [];
+// corrida.participantes[0] = new Carro("kicks", 120, 160, 5);
+// corrida.participantes[1] = new Carro("marea", 210, 200, 9); // Corrigido o índice
+// corrida.participantes[2] = new Carro("peugeot 206", 300, 220, 10); // Corrigido o índice
+// 
+// corrida.DefinirVencedor();
+// corrida.ExibirVencedor();
